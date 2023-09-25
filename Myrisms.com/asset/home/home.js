097f1a0e1a9ecd6f8fe9db33e5e47c44d5f852a3
athallah19 <athallahabhi999@gmail.com>
2023-09-26 00:16:17 +0700

    update animasi

--- Myrisms.com/asset/home/home.js
+++ Myrisms.com/asset/home/home.js
@@ -29,3 +29,4 @@ function dropdown() {
         $('#hambuger').attr('src','asset/hambuger2.png')
     }
 }
+
--- Myrisms.com/asset/my.js
+++ Myrisms.com/asset/my.js
@@ -14,3 +14,4 @@ const onScroll = (event) => {
 };
 
 document.addEventListener("scroll", onScroll);
+
