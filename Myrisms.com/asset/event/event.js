f1dc55797641b984e601cccbb08a05f798210072
athallah19 <athallahabhi999@gmail.com>
2023-09-25 15:43:52 +0700

    update

--- Myrisms.com/asset/event/event.js
+++ Myrisms.com/asset/event/event.js
@@ -2,9 +2,9 @@ function dropdown() {
     let status = $('#dropdown').css('display')
     if (status == 'block') {
         $('#dropdown').hide()
-        $('#hambuger').attr('src','asset/hambuger1.png')
+        $('#hambuger').attr('src','../asset/hambuger1.png')
     } else {
         $('#dropdown').show()
-        $('#hambuger').attr('src','asset/hambuger2.png')
+        $('#hambuger').attr('src','../asset/hambuger2.png')
     }
 }
