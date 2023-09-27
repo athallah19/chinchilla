5d5591971531bd514c3178bda3586a240de24453
arramasiraa <rhmaw195@gmail.com>
2023-09-27 10:55:16 +0700

    update

--- Myrisms.com/asset/my.js
+++ Myrisms.com/asset/my.js
@@ -13,5 +13,19 @@ const onScroll = (event) => {
   }
 };
 
-document.addEventListener("scroll", onScroll);
+// document.addEventListener("scroll", onScroll);
+
+// document.onreadystatechange = function () {
+//   if (document.readyState !== "complete") {
+//     document.querySelector(
+//       "body").style.visibility = "hidden";
+//     document.querySelector(
+//       "#loader").style.visibility = "visible";
+//   } else {
+//     document.querySelector(
+//       "#loader").style.display = "none";
+//     document.querySelector(
+//       "body").style.visibility = "visible";
+//   }
+// };
 
