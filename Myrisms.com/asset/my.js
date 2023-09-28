d1e728868978cfdca475e6356b0db96921a9802c
athallah19 <athallahabhi999@gmail.com>
2023-09-28 12:43:35 +0700

    loading animation

--- Myrisms.com/asset/my.js
+++ Myrisms.com/asset/my.js
@@ -13,19 +13,9 @@ const onScroll = (event) => {
   }
 };
 
-// document.addEventListener("scroll", onScroll);
+document.addEventListener("scroll", onScroll);
 
-// document.onreadystatechange = function () {
-//   if (document.readyState !== "complete") {
-//     document.querySelector(
-//       "body").style.visibility = "hidden";
-//     document.querySelector(
-//       "#loader").style.visibility = "visible";
-//   } else {
-//     document.querySelector(
-//       "#loader").style.display = "none";
-//     document.querySelector(
-//       "body").style.visibility = "visible";
-//   }
-// };
 
+$(document).ready(function () {
+  $(".bg-loader").hide();
+})
